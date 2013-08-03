f4924dd5de82671de6ec9b1e36ba3c65e4309ea7
Matías Muñoz Espinoza <writkas2@gmail.com>
2013-08-02 20:49:47 -0400

    Añadiendo archivos y algo más

---
+++ src/nave3000/Score.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nave3000;
+
+/**
+ *
+ * @author writkas
+ */
+public class Score {
+    
+}
