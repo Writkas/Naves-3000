e54d6dae87ac6488351df775605a5ba2a7c0be4e
Patricio <patricio.saez69@gmail.com>
2013-08-05 17:37:44 -0400

    añadindo a la clase World

--- src/nave3000/World.java
+++ src/nave3000/World.java
@@ -16,5 +16,14 @@ public class World extends Entity {
           Point bottomRight, Point bottomLeft) {
       this.upperLeft.x = upperLeft.x;
       this.upperLeft.y = upperLeft.y;
+      this.upperRight.x= upperRight.x;
+      this.upperRight.y= upperRight.y;
+      this.bottomRight.x= bottomRight.x;
+      this.bottomRight.y= bottomRight.y;
+      this.bottomLeft.x= bottomLeft.x;
+      this.bottomLeft.y= bottomLeft.y;
+      
+        
+      
    }
 }
